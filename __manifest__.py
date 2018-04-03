9e3a53a29beffcaaa0a720bfd5d6ab4a845d070a
Adrian Chang <achang@aim-rg.com>
2018-04-03 15:09:09 -0400

    @http.route('/dependencies_graph/<string:module_name>', type='json', auth="none")

--- __manifest__.py
+++ __manifest__.py
@@ -3,33 +3,32 @@
     'name': "dependencies_graph",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Visualize the dependencies graph for Odoo modules and JavaScript services""",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    # 'description': """
+    #     Long description of module's purpose
+    # """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Adrian Chang",
+    # 'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Technical Settings',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # # always loaded
+    # 'data': [
+    #     # 'security/ir.model.access.csv',
+    #     'views/views.xml',
+    #     'views/templates.xml',
+    # ],
+    # # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
 }
--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
+import json
 from odoo import http
 
-# class DependenciesGraph(http.Controller):
+import odoo
+import odoo.modules.db
+import odoo.modules.graph
+from odoo.modules.registry import Registry
+
+
+class DependenciesGraph(http.Controller):
+    @http.route('/dependencies_graph/<string:module_name>', type='json', auth="none")
+    def get_graph(self, module_name):
+        dbname = odoo.tools.config['db_name']
+        cr = Registry.new(dbname)._db.cursor()
+        graph = odoo.modules.graph.Graph()
+
+        states = ['installed', 'to upgrade', 'to remove']
+        cr.execute("SELECT name from ir_module_module WHERE state IN %s", (tuple(states),))
+        module_list = [name for (name,) in cr.fetchall() if name not in graph]
+
+        graph.add_modules(cr, module_list)
+        if module_name is graph:
+            graph = graph[module_name]
+
+        deps = {}
+        for key, value in graph.iteritems():
+            deps[key] = value.info['depends']
+
+        return json.dumps(deps)
+    
 #     @http.route('/dependencies_graph/dependencies_graph/', auth='public')
 #     def index(self, **kw):
 #         return "Hello, world"
