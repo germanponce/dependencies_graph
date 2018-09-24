70c7ecc254f7992b2a1b715cbdeaac9d7fa21f7c
Adrian Chang <adrian.chang.alcover@gmail.com>
2018-09-24 17:24:57 -0400

    removing module graph and js graph

--- dependencies_graph/static/src/js/graph.js
+++ dependencies_graph/static/src/js/graph.js
@@ -175,85 +175,7 @@ odoo.define('dependencies_graph.graph', function (require) {
         return promise;
     };
 
-    w.module_graph = function (module, keywords) {
-        var promise = $.Deferred();
-        session.rpc('/dependencies_graph/modules').done(function (result) {
-            var deps = JSON.parse(result);
-            var nodes = new vis.DataSet([]);
-            var edges = new vis.DataSet([]);
-
-            _.each(_.keys(deps), function (dep) {
-                if (filter(dep, keywords) || dep === module) {
-                    nodes.add({id: dep, label: dep});
-                }
-            });
-
-            _.each(deps, function (value, key) {
-                _.each(value, function (dep) {
-                    if (filter(key, keywords) && filter(dep, keywords) || dep === module) {
-                        edges.add({from: dep, to: key, arrows: 'to'})
-                    }
-                })
-            });
-
-            // create a network
-            var container = $(selector)[0];
-            var data = {
-                nodes: nodes,
-                edges: edges
-            };
-            options['configure']['container'] = $('#settings')[0];
-            var network = new vis.Network(container, data, options);
-
-            promise.resolve(network);
-        });
-        return promise;
-    };
-
-    w.js_graph = function (module, keywords) {
-        var promise = $.Deferred();
-        var nodes = new vis.DataSet([]);
-        var edges = new vis.DataSet([]);
-
-        _.each(window.odoo.__DEBUG__.services, function (value, key) {
-            if (typeof value === 'function' && filter(key, keywords)) {
-                nodes.add({id: key, label: key, value: value})
-            }
-            if (typeof value === 'object') {
-                _.each(value, function (v, k) {
-                    if (typeof v === 'function') {
-                        var name = key.concat('.', k);
-                        if (filter(name, keywords)) {
-                            nodes.add({id: name, label: name, value: v});
-                        }
-                    }
-                })
-            }
-        });
-
-        _.each(nodes._data, function (x) {
-            _.each(nodes._data, function (y) {
-                if (x['value'].prototype && x['value'].prototype.__proto__.constructor === y['value']) {
-                    edges.add({from: y['id'], to: x['id'], arrows: 'to'})
-                }
-            })
-
-        });
-
-        // create a network
-        var container = $(selector)[0];
-        var data = {
-            nodes: nodes,
-            edges: edges
-        };
-        options['configure']['container'] = $('#settings')[0];
-        var network = new vis.Network(container, data, options);
-
-        promise.resolve(network);
-        return promise;
-    };
-
-    w.js_parents = function (module, keywords) {
+    w.js_parents = function (module) {
         var promise = $.Deferred();
         var nodes = new vis.DataSet([]);
         var edges = new vis.DataSet([]);
@@ -290,7 +212,7 @@ odoo.define('dependencies_graph.graph', function (require) {
         return promise;
     };
 
-    w.js_children = function (module, keywords) {
+    w.js_children = function (module) {
         var promise = $.Deferred();
         var nodes = new vis.DataSet([]);
         var edges = new vis.DataSet([]);
