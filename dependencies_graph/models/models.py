5fdd91a1a3af4b0e87dd3d7eb338bc784ecaddd1
Adrian Chang <adrian.chang.alcover@gmail.com>
2018-09-26 19:44:17 -0400

    creates dependencies_graph_settings model for loading external JS assets from many modules

--- dependencies_graph/models/models.py
+++ dependencies_graph/models/models.py
@@ -2,14 +2,8 @@
 
 from odoo import models, fields, api
 
-# class dependencies_graph(models.Model):
-#     _name = 'dependencies_graph.dependencies_graph'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class dependencies_graph_settings(models.Model):
+    _name = 'dependencies_graph.settings'
+
+    module_ids = fields.Many2many('ir.module.module', 'dependencies_graph_module_dependency', 'base', 'module_id')
