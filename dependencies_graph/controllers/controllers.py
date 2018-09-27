d33032cefc65accc183a84844ddfc729d165319b
Adrian Chang <adrian.chang.alcover@gmail.com>
2018-09-26 21:56:00 -0400

    load external JS assets returned by index

--- dependencies_graph/controllers/controllers.py
+++ dependencies_graph/controllers/controllers.py
@@ -11,7 +11,24 @@ class DependenciesGraph(http.Controller):
 
     @http.route('/dependencies_graph/graph/', auth='user')
     def index(self, **kw):
-        return http.request.render('dependencies_graph.graph')
+        modules = http.request.env.ref('dependencies_graph.basic_settings').module_ids.mapped('name')
+
+        cr = http.request.cr
+        query = """SELECT arch_db
+                     FROM ir_ui_view v
+                LEFT JOIN ir_model_data md ON (md.model = 'ir.ui.view' AND md.res_id = v.id)"""
+        cr.execute(query + 'WHERE md.module IN %s', (tuple(modules),))
+        views = cr.fetchall()
+        scripts = []
+
+        for (view,) in views:
+            root = ET.fromstring(view)
+            for script in root.iter('script'):
+                if 'src' in script.attrib:
+                    scripts.append(script.attrib['src'])
+        scripts = list(set(scripts))
+
+        return http.request.render('dependencies_graph.graph', {'scripts': scripts})
 
     @http.route('/dependencies_graph/modules', type='json', auth='user')
     def get_graph(self):
